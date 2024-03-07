14af42d48ba62c3ad5d46075c032a6594b0969b5
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-03-07 08:23:51 -0600

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Jackson Harrison on 3/7/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
